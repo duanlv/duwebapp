1d26180c3a6ec0c1b6b51cd32d3e7a025da75949
Le Van Duan <duanlv89@gmail.com>
2018-06-01 23:25:00 +0700

    try url routes

--- app.py
+++ app.py
@@ -21,8 +21,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return "index!"
+
+@app.route("/hello")
 def hello():
-    return "Hello World!"
+    return "Hello Word!"
+
+@app.route("/members")
+def hello():
+    return "Members"
+
+@app.route("/members/<string:name>/")
+def getMember(name):
+    return name
 
 if __name__ == "__main__":
     app.run()
