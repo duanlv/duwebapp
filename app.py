b80fc5e147277e2e314e20c4b9ab00d1b4058647
Le Van Duan <duanlv89@gmail.com>
2018-06-02 00:19:58 +0700

    revert change

--- app.py
+++ app.py
@@ -2,36 +2,37 @@ from flask import Flask
 from datetime import datetime
 app = Flask(__name__)
 
-# @app.route('/')
-# def homepage():
-    # the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-#
-    # return """
-    # <h1>Hello heroku</h1>
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+
     # <p>It is currently {time}.</p>
-#
+
     # <img src="http://loremflickr.com/600/400" />
     # """.format(time=the_time)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
+
 #
-# if __name__ == '__main__':
-    # app.run(debug=True, use_reloader=True)
+# @app.route("/")
+# def index():
+    # return "index!"
 #
-
-@app.route("/")
-def index():
-    return "index!"
-
-@app.route("/hello")
-def hello():
-    return "Hello Word!"
-
-@app.route("/members")
-def hello():
-    return "Members"
-
-@app.route("/members/<string:name>/")
-def getMember(name):
-    return name
-
-if __name__ == "__main__":
-    app.run()
+# @app.route("/hello")
+# def hello():
+    # return "Hello Word!"
+#
+# @app.route("/members")
+# def hello():
+    # return "Members"
+#
+# @app.route("/members/<string:name>/")
+# def getMember(name):
+    # return name
+#
+# if __name__ == "__main__":
+    # app.run()
